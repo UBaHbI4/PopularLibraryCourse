caea7bc1faa8e5f14c311b12cf072372450de184
UBaHbI4 <kinoshnik_ivan@mail.ru>
2021-07-29 14:39:54 +0700

    - Исправлена ошибка в презентере. Разделил метод Count на 3 метода, чтобы избавиться от индекса. Насколько это правильно? Как действовать, когда неизвестно количество кнопок например? - Сделал сохранение состояния при повороте экрана.

--- app/src/main/java/softing/ubah4ukdev/popularlibrary/Presenter.kt
+++ app/src/main/java/softing/ubah4ukdev/popularlibrary/Presenter.kt
@@ -1,10 +1,8 @@
 package softing.ubah4ukdev.popularlibrary
 
-import softing.ubah4ukdev.populatelibrary.R
-
 /****
 Project PopulateLibrary
-Package softing.ubah4ukdev.populatelibrary
+Package softing.ubah4ukdev.popularlibrary
 
 Created by Ivan Sheynmaer
 
@@ -19,17 +17,15 @@ class Presenter(private val view: IView) : IPresenter {
         view.showCounter3("${Model[2]}")
     }
 
-    override fun count(buttonId: Int) {
-        when (buttonId) {
-            R.id.btn_counter1 -> {
-                view.showCounter1("${++Model[0]}")
-            }
-            R.id.btn_counter2 -> {
-                view.showCounter2("${++Model[1]}")
-            }
-            R.id.btn_counter3 -> {
-                view.showCounter3("${++Model[2]}")
-            }
-        }
+    override fun count1() {
+        view.showCounter1("${++Model[0]}")
+    }
+
+    override fun count2() {
+        view.showCounter2("${++Model[1]}")
+    }
+
+    override fun count3() {
+        view.showCounter3("${++Model[2]}")
     }
 }
